1d00869e0dcb751c5c66fdbaf31ec8f0c4733b83
leeyc924 <leeyc924@naver.com>
2024-01-27 22:29:31 +0900

    init first commit

---
+++ packages/ui/src/components/Button/index.tsx
@@ -0,0 +1,36 @@
+import {
+  ComponentPropsWithRef,
+  ComponentPropsWithoutRef,
+  ElementType,
+  ReactNode,
+} from "react";
+
+export interface ButtonProps {
+  children: ReactNode;
+}
+
+type PolymorphicComponentProps<
+  T extends ElementType,
+  Props = Record<string, unknown>,
+> = { component?: T } & ComponentPropsWithoutRef<T> &
+  Props & {
+    ref?: ComponentPropsWithRef<T>["ref"];
+  };
+
+const Button = <T extends ElementType = "button">({
+  component,
+  children,
+  ...otherProps
+}: PolymorphicComponentProps<T, ButtonProps>) => {
+  const Component = component || "button";
+  return (
+    <Component
+      {...(Component === "button" && { type: "button" })}
+      {...otherProps}
+    >
+      {children}
+    </Component>
+  );
+};
+
+export default Button;
