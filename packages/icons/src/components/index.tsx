02a2c75c7b7602d89b2b551b74dccac7abfad2bf
leeyc924 <leeyc924@naver.com>
2024-02-13 13:02:35 +0900

    Modify icon version 0.0.4

--- packages/icons/src/components/index.tsx
+++ packages/icons/src/components/index.tsx
@@ -17,7 +17,20 @@ const Icon = ({ color = '#000', irName, name, size = 24 }: IconProps) => {
         color,
       }}
     >
-      {irName && <span>{irName}</span>}
+      {irName && (
+        <span
+          style={{
+            position: 'absolute',
+            overflow: 'hidden',
+            width: '1px',
+            height: '1px',
+            border: 0,
+            clip: `rect(1px, 1px, 1px, 1px)`,
+          }}
+        >
+          {irName}
+        </span>
+      )}
     </i>
   );
 };
--- packages/icons/src/constants/index.ts
+++ packages/icons/src/constants/index.ts
@@ -1,3 +1,19 @@
-const icons = ['arrow_down', 'arrow_left', 'arrow_right', 'arrow_up'] as const;
+const icons = [
+  'arrow_down',
+  'arrow_left',
+  'arrow_right',
+  'arrow_up',
+  'calendar',
+  'check',
+  'close',
+  'delete',
+  'edit',
+  'grid',
+  'home',
+  'human',
+  'list',
+  'minus',
+  'plus',
+] as const;
 
 export default icons;
