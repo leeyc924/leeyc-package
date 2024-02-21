5867d21ab9679535b7f998fb0f4453b887bc54ed
leeyc924 <leeyc924@naver.com>
2024-02-21 23:50:22 +0900

    Add TabPanel

--- apps/storybook/stories/Components/Tabs.stories.tsx
+++ apps/storybook/stories/Components/Tabs.stories.tsx
@@ -1,5 +1,6 @@
 import { Tabs, TabsProps } from '@components';
-import { Meta, StoryObj } from '@storybook/react';
+import { Meta, StoryFn } from '@storybook/react';
+import { useState } from 'react';
 
 const story: Meta<TabsProps> = {
   component: Tabs,
@@ -9,8 +10,28 @@ const story: Meta<TabsProps> = {
 
 export default story;
 
-export const Default: StoryObj<TabsProps> = {
-  args: {
-    children: 'Tabs',
-  },
+export const Default: StoryFn<TabsProps> = () => {
+  const [id, setId] = useState('1');
+  return (
+    <>
+      <Tabs selectedTabId={id} onClick={tabId => setId(tabId)}>
+        <Tabs.TabList
+          tabItemList={[
+            { tabId: '1', label: '1' },
+            { tabId: '2', label: '2' },
+            { tabId: '3', label: '3' },
+          ]}
+        />
+        <Tabs.TabPanel label="1번탭" tabId="1">
+          1번탭
+        </Tabs.TabPanel>
+        <Tabs.TabPanel label="2번탭" tabId="2">
+          2번탭
+        </Tabs.TabPanel>
+        <Tabs.TabPanel label="3번탭" tabId="3">
+          3번탭
+        </Tabs.TabPanel>
+      </Tabs>
+    </>
+  );
 };
--- packages/ui/src/components/Tabs/index.css.ts
+++ packages/ui/src/components/Tabs/index.css.ts
@@ -1,4 +1,17 @@
-import { style } from '@vanilla-extract/css';
+import { style, styleVariants } from '@vanilla-extract/css';
 import { palette } from '../../styles';
 
-export const container = style({});
+export const container = style({
+  display: 'flex',
+  alignItems: 'center',
+});
+
+export const tabItem = styleVariants({
+  base: {
+    flex: 1,
+    borderBottom: `1px solid ${palette.Gray700}`,
+  },
+  select: {
+    borderBottom: `1px solid ${palette.Primary}`,
+  },
+});
