84d32739a297fd9ff4b6998969c39688347db84e
leeyc924 <leeyc924@naver.com>
2024-09-19 23:45:15 +0900

    test typia

--- apps/react-test/src/App.tsx
+++ apps/react-test/src/App.tsx
@@ -1,7 +1,20 @@
 import { Button } from '@breadlee/ui';
-import { useState } from 'react';
+import typia, { tags } from 'typia';
+
+interface IMember {
+  id: string;
+  email: string & tags.Format<'email'>;
+  age: number & tags.Type<'uint32'> & tags.ExclusiveMinimum<19> & tags.Maximum<100>;
+}
 
 function App() {
+  const matched: boolean = typia.is<IMember>({
+    id: '1234',
+    email: 'samchon.github@gmai19l.com',
+    age: 30,
+  });
+
+  console.log(matched); // true
   return <Button>test</Button>;
 }
 
