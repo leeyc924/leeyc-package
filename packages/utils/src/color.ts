5b84d5fa9cf16794c653ae23e4803297ec3bb017
leeyc924 <leeyc924@naver.com>
2024-11-13 10:58:05 +0900

    modify: color util

--- packages/utils/src/color.ts
+++ packages/utils/src/color.ts
@@ -1,14 +1,9 @@
-export function rgbaToHex({ a, b, g, r }: { r: number; g: number; b: number; a: number }) {
-  const toHex = (n: number) => {
-    const hex = n.toString(16);
-    return hex.length === 1 ? '0' + hex : hex;
-  };
+export function hexToRgbaObject(hex: string) {
+  if (!/^#[0-9A-Fa-f]{3,8}$/.test(hex)) {
+    console.log('유효하지 않은 HEX 값입니다');
+    return { r: 0, g: 0, b: 0, a: 0 };
+  }
 
-  const alpha = Math.round(a * 255);
-  return `#${toHex(r)}${toHex(g)}${toHex(b)}${toHex(alpha)}`;
-}
-
-export function hexToRgbaString(hex: string) {
   hex = hex.replace(/^#/, '');
 
   if (hex.length === 3) {
@@ -18,15 +13,14 @@ export function hexToRgbaString(hex: string) {
       .join('');
   }
 
-  // 8자리 HEX 값의 경우, 알파 값을 올바르게 추출하기 위해 순서를 조정합니다.
   if (hex.length === 8) {
-    const a = Number((parseInt(hex.slice(6, 8), 16) / 255).toFixed(1));
+    const a = Number((parseInt(hex.slice(6, 8), 16) / 255).toFixed(2));
     hex = hex.slice(0, 6);
     const bigint = parseInt(hex, 16);
     const r = (bigint >> 16) & 255;
     const g = (bigint >> 8) & 255;
     const b = bigint & 255;
-    return `rgba(${r}, ${g}, ${b}, ${a})`;
+    return { r, g, b, a };
   }
 
   const bigint = parseInt(hex, 16);
@@ -34,12 +28,23 @@ export function hexToRgbaString(hex: string) {
   const g = (bigint >> 8) & 255;
   const b = bigint & 255;
 
-  return `rgba(${r}, ${g}, ${b}, 1)`;
+  return { r, g, b, a: 1 };
 }
 
-export function rgbaToString(rgba: { r: number; g: number; b: number; a: number }, isAlpha = true) {
+export function rgbaToHex({ a, b, g, r }: { r: number; g: number; b: number; a: number }) {
+  const toHex = (n: number) => {
+    const hex = n.toString(16);
+    return hex.length === 1 ? '0' + hex : hex;
+  };
+
+  const alpha = Math.round(a * 255);
+
+  return `#${toHex(r)}${toHex(g)}${toHex(b)}${toHex(alpha)}`;
+}
+
+export function rgbaToString(rgba: { r: number; g: number; b: number; a: number }) {
   const { a, b, g, r } = rgba;
-  return `rgba(${r}, ${g}, ${b}, ${isAlpha ? a : 1})`;
+  return `rgba(${r}, ${g}, ${b}, ${a})`;
 }
 
 export function rgbaStringToObject(rgba: string) {
